ed27fa1c1965e4f62183a9d7f02b7d4c191f3c40
marina.grigorieva <grigorievamn87@mail.ru>
2021-02-20 12:52:39 +0300

    chat

---
+++ hw6/src/ru/geekbrains/courses/java_2/hw6/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.courses.java_2.hw6;
+
+public class Main {
+    public static void main(String[] args) {
+        //Server.startServer();
+        Client.startClient();
+
+    }
+}
